f9d02a34064d429fcf18aa508fbc561869c615ba
Sebastian Gierlinger <s.gierlinger@me.com>
2020-06-10 18:09:16 +0200

    Trigger Github Build

--- api/StoryblokWebhook/index.js
+++ api/StoryblokWebhook/index.js
@@ -1,16 +1,40 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    // curl -X POST https://api.github.com/repos/:owner/:repo/dispatches \
+    // -H 'Accept: application/vnd.github.everest-preview+json' \
+    // -H 'Authorization: token $TOKEN' \
+    // --data '{"event_type": "$CUSTOM_ACTION_NAME"}'
+    context.log('Process Storyblok Webhook')
+    context.log()
+    const owner = 'sebgie'
+    const repo = 'vue-nuxt-boilerplate'
+    const url = `https://api.github.com/repos/${owner}/${repo}/dispatches`
+    const requestBody = {
+        event_type: 'storyblok_publish'
+    }
+    const config = {
+        headers: {
+            'Accept': 'application/vnd.github.everest-preview+json',
+            'Authorization': `token ${process.env.GITHUB_TOKEN}`
+        }
+    }
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: process.env.GITHUB_TOKEN //"Hello " + (req.query.name || req.body.name)
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
+    axios.post(url, requestBody, config)
+        .then((response) => {
+            context.log(response)
+            context.res = {
+                // status: 200, /* Defaults to 200 */
+                body: {
+                    success: true
+                }
+            };
+        })
+        .catch((err) => {
+            context.log(err)
+            context.res = {
+                status: 400,
+                body: {
+                    success: false
+                }
+            };
+        })
 };
